8f22045ab48225c70fe975473df71b32f6d094e4
Magnus Maichle <magnus.maichle@uni-wuerzburg.de>
2024-08-05 14:42:51 +0200

    changed logic to update lÃag window

--- ddopnew/dataloaders/tabular.py
+++ ddopnew/dataloaders/tabular.py
@@ -10,6 +10,8 @@ from typing import Union
 
 from .base import BaseDataLoader
 
+from sklearn.preprocessing import StandardScaler
+
 # %% ../../nbs/10_dataloaders/12_tabular_dataloaders.ipynb 4
 class XYDataLoader(BaseDataLoader):
 
@@ -27,7 +29,8 @@ class XYDataLoader(BaseDataLoader):
         Y: np.ndarray,
         val_index_start: Union[int, None] = None, 
         test_index_start: Union[int, None] = None, 
-        lag_window_params: Union[dict] = None # default: {'lag_window': 0, 'include_y': False, 'pre_calc-calc': False}
+        lag_window_params: Union[dict] = None, # default: {'lag_window': 0, 'include_y': False, 'pre_calc-calc': False}
+        normalize_features: Union[dict] = None, # default: {'normalize': True, 'ignore_one_hot': True}
     ):
 
         self.X = X
@@ -46,8 +49,10 @@ class XYDataLoader(BaseDataLoader):
 
         self.dataset_type = "train"
 
+        normalize_features = normalize_features or {'normalize': True, 'ignore_one_hot': True}
         lag_window_params = lag_window_params or {'lag_window': 0, 'include_y': False, 'pre_calc': False}
 
+        self.normalize_features(**normalize_features, initial_normalization=True)
         self.prep_lag_features(**lag_window_params)
 
         # X must at least have datapoint and feature dimension
@@ -63,6 +68,41 @@ class XYDataLoader(BaseDataLoader):
         self.num_units = Y.shape[1] # shape 0 is alsways time, shape 1 is the number of units (e.g., SKUs)
 
         super().__init__()
+
+    def normalize_features(self,
+        normalize: bool = True,
+        ignore_one_hot: bool = True,
+        initial_normalization=False # Flag if it is set before having added lag features
+        ):
+
+        """
+        Normalize features using a standard scaler. If ignore_one_hot is true, one-hot encoded features are not normalized.
+
+        """
+
+        if normalize:
+
+            scaler = StandardScaler()
+
+            if initial_normalization:
+
+                if len(self.X.shape) == 3:
+                    raise ValueError('Normalization not possible with lag features. Please set initial_normalization=False')
+            
+                scaler.fit(self.X[:self.train_index_end+1]) # +1 to include the last training point
+                scaler.transform(self.X)
+
+                if initial_normalization:
+                    return
+                else:
+                    raise NotImplementedError('Normalization after lag features have been set not implemented yet')
+
+                    # Idea:
+                        # remove time dimension
+                        # normalize features
+                        # add time_dimension back
+                    # Problem:
+                        # usage of prep_lag_features needs to ensure y is not added a second time
 
     def prep_lag_features(self,
         lag_window: int = 0, # length of the lage window
@@ -80,9 +120,9 @@ class XYDataLoader(BaseDataLoader):
         """
         # to be discussed: Do we need option to only provide lag demand wihtout lag features?
         self.lag_window = lag_window
-        self.include_y = include_y
         self.pre_calc = pre_calc
-
+        self.include_y = include_y
+        
         if self.pre_calc:
             if self.include_y:
                 # add additional column to X with demand shifted by 1
@@ -114,9 +154,18 @@ class XYDataLoader(BaseDataLoader):
         else:
             self.lag_window = None
             self.include_y = False
+            # add time dimension to X
 
-                # add time dimension to X
-    
+    def update_lag_features(self,
+        lag_window: int,
+        ):
+
+        """ Update lag window parameters for dataloader object that is already initialized """
+
+        raise NotImplementedError('Not implemented yet')
+
+        # Problem: updating lag_features naively would shorten the dataset each time it is called
+
     def __getitem__(self, idx): 
 
         """ get item by index, depending on the dataset type (train, val, test)"""
